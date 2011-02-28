876345fa23396409f2396fb2a8d3df5129d2367c
Qianlong Zhao <qianlongzhao@gmail.com>
2011-02-28 09:58:15 +0100

    initial de ruby projet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyProjet::Application.config.secret_token = '3285c60faf507d282438816f8c3a89a9bde7859eebc17db3341c67e7ff4a99a192d94453f69014f2daeef06ac6ca29dcaa36c24cb6509fb20de9244c09944bd7'
